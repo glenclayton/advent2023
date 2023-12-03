5f52cfd78e0ffcff3504c696be0519fddfe37a9c
Glen Clayton <glen.clayton@gmail.com>
2023-12-03 17:34:47 +0000

    part1 works

--- test_day03.py
+++ test_day03.py
@@ -2,18 +2,33 @@ import day03
 
 def data():
     test_data= [
-        "467..114..",
-        "...*......",
-        "..35..633.",
-        "......#...",
-        "617*......",
-        ".....+.58.",
-        "..592.....",
-        "......755.",
-        "...$.*....",
-        ".664.598.."
+        "467..114..\n",
+        "...*......\n",
+        "..35..633.\n",
+        "......#...\n",
+        "617*......\n",
+        ".....+.58.\n",
+        "..592.....\n",
+        "......755.\n",
+        "...$.*....\n",
+        ".664.598..\n"
     ]
     return test_data
+
+def data2():
+    return [
+        "12.......*..",
+        "+.........34",
+        ".......-12..",
+        "..78........",
+        "..*....60...",
+        "78.........9",
+        ".5.....23..$",
+        "8...90*12...",
+        "............",
+        "2.2......12.",
+        ".*.........*",
+        "1.1..503+.56"]
 
 def test_inputToArray():
     test_data=['12','34']
@@ -59,3 +74,43 @@ def test_processRowNumbers():
     assert numbers[0].x == 1
     assert numbers[1].number == 34
     assert numbers[1].x == 4
+    test_data = '34$'
+    _, numbers = day03.processRow(test_data,0)
+    assert len(numbers) == 1
+    assert numbers[0].number == 34
+    assert numbers[0].x == 0
+    test_data = '..34'
+    _, numbers = day03.processRow(test_data,0)
+    assert len(numbers) == 1
+    assert numbers[0].number == 34
+    assert numbers[0].x == 2
+
+def test_boundayBox():
+    agame = day03.number(123,0,0)
+    assert len(agame.box) == 5
+    assert agame.box[(0,1)] == True
+    assert agame.box.get((4,0)) == None
+    assert agame.box[(3,0)] == True
+    agame = day03.number(1,1,1)
+    assert len(agame.box) == 8
+    assert agame.box.get((1,1)) == None
+    assert agame.box.get((0,0)) ==True
+    assert agame.box.get((2,2)) ==True
+    assert agame.box.get((0,1)) ==True
+    assert agame.box.get((2,1)) ==True
+    agame= day03.number(789,7,1)
+    assert len(agame.box) == 9
+    assert agame.box.get((6,1)) ==True
+    assert agame.box.get((6,0)) ==True
+    assert agame.box.get((6,2)) ==True
+    assert agame.box.get((7,2)) ==True
+
+def test_block():
+    test_data=data()
+    answer,dd = day03.findAndSum(test_data)
+    assert answer == 4361
+
+def test_block2():
+    test_data=data2()
+    answer, dd = day03.findAndSum(test_data)
+    assert answer == 925
