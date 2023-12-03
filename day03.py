4ad90ef002417a50cf865d92b1ae6aa987268e64
Glen Clayton <glen.clayton@gmail.com>
2023-12-03 08:45:58 +0000

    row parsing works

--- day03.py
+++ day03.py
@@ -9,3 +9,51 @@ def inputToArray(input):
         rv.append(therow)    
     return rv
 
+def isSymbol(s):
+    return s in '$*#'
+
+class symbol:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+class number:
+    def __init__(self,number,x,y):
+        self.number=number
+        self.x=x
+        self.y=y
+        self.xe=x+len(str(number))-1
+
+
+def processRow(input,y):
+    numbers = []
+    symbols = []
+    nm = False
+    ns = ''
+    nstart = -1
+    for i in range(0,len(input)):
+        s = input[i]
+        if s.isdigit():
+            if not nm:
+                nstart = i
+            ns=ns+s
+            nm = True
+        elif isSymbol(s):
+            sym = symbol(i,y)
+            symbols.append(sym)
+            if nm:
+                num = number(int(ns),nstart,y)
+                numbers.append(num)
+                nm = False
+                ns = ''
+        elif nm:
+            num = number(int(ns),nstart,y)
+            numbers.append(num)
+            nm = False
+            ns = ''
+    if nm:
+        num = number(int(ns),nstart,y)
+        numbers.append(num)
+
+    return symbols, numbers
+
--- test_day03.py
+++ test_day03.py
@@ -22,3 +22,40 @@ def test_inputToArray():
     assert array[0][1] == '2'
     assert array[1][0] == '3'
     assert array[1][1] == '4'
+
+def test_isSymbol():
+    assert day03.isSymbol('$') == True
+    assert day03.isSymbol('.') == False
+
+def test_processRowSymbols():
+    test_data = '..$.*.#'
+    symbols, _ = day03.processRow(test_data,0)
+    assert len(symbols) == 3
+    assert symbols[0].x == 2
+    assert symbols[0].y == 0
+
+def test_processRowNumbers():
+    test_data = '1'
+    _, numbers = day03.processRow(test_data,0)
+    assert numbers[0].number == 1
+    test_data = '12'
+    _, numbers = day03.processRow(test_data,0)
+    assert numbers[0].number == 12
+    test_data = '.12.'
+    _, numbers = day03.processRow(test_data,0)
+    assert numbers[0].number == 12
+    assert numbers[0].x == 1
+    test_data = '.12.34'
+    _, numbers = day03.processRow(test_data,0)
+    assert len(numbers) == 2
+    assert numbers[0].number == 12
+    assert numbers[0].x == 1
+    assert numbers[1].number == 34
+    assert numbers[1].x == 4
+    test_data = '.12$34'
+    _, numbers = day03.processRow(test_data,0)
+    assert len(numbers) == 2
+    assert numbers[0].number == 12
+    assert numbers[0].x == 1
+    assert numbers[1].number == 34
+    assert numbers[1].x == 4
