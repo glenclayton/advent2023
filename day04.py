7516103a51b8bc56307aec4184631bad98475b79
Glen Clayton <glen.clayton@gmail.com>
2023-12-04 16:03:02 +0000

    next

--- day04.py
+++ day04.py
@@ -8,18 +8,56 @@ class card:
         cardno = int(s1[0][4:])
         s2 = s1[1].split('|')
         ws = ' '+s2[0].strip()
-        print(f"A{ws}A")
+    #print(f"A{ws}A")
         ms = s2[1]
         winners = []
         my = []
         for i in range(0,len(ws)//3):
-            print(f"A{ws[i*3:i*3+3]}A")
+         #   print(f"A{ws[i*3:i*3+3]}A")
             winners.append(int(ws[i*3:i*3+3]))
         for i in range(0,len(ms)//3):
-            print(f"A{ms[i*3:i*3+3]}A")
+          #  print(f"A{ms[i*3:i*3+3]}A")
             my.append(int(ms[i*3:i*3+3]))
         return cardno,winners,my
-        
+    
+    def points(self):
+        num = 0
+        powe = 0
+        for winner in self.winners:
+            if winner in self.yours:
+                num = num+1
+        if num>0:
+            powe = pow(2,num-1)
+        return powe
+
+    def checkDedup(self):
+        dw = list(dict.fromkeys(self.winners))
+        dy = list(dict.fromkeys(self.yours))
+        a = False
+        b = False
+        if len(dw) != len(self.winners):
+            a = True
+        if len(dy) != len(self.yours):
+            b = True
+        return a,b
+
     def __init__(self, line):
         self.num, self.winners, self.yours = card.parse(line)
-    
+
+def compute():
+    text_file = open("data/input_day04.txt", "r")
+    lines = text_file.readlines()
+    print(lines)
+    print(len(lines))
+    text_file.close()
+    sum=0
+    for line in lines:
+        mycard=card(line)
+        sum=sum+mycard.points()
+        a,b = mycard.checkDedup()
+        if a or b:
+            print(f"card {mycard.num} winner {a} yours {b}")
+    print(f"Answer is {sum}")
+
+compute()
+
--- test_day04.py
+++ test_day04.py
@@ -9,10 +9,11 @@ def data():
         "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
         "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
         "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
-    ]
+    ],[8,2,2,1,0,0]
 
 def test_parse():
-    test_data = data()[0]
+    dat,_ = data()
+    test_data = dat[0]
     no, winners, my = day04.card.parse(test_data)
     assert no == 1
     assert len(winners) == 5 
@@ -21,3 +22,28 @@ def test_parse():
     assert len(my) == 8
     assert my[0] == 83
     assert my[2] == 6
+
+def test_pow_single():
+    dat,_ = data()
+    test_data = dat[0]
+    mycard = day04.card(test_data)
+    assert mycard.points() == 8
+
+def test_pow_multiplie():
+    dat, ans = data()
+    rsum = 0
+    for l,a in zip(dat, ans):
+        mycard = day04.card(l)
+        assert mycard.points() == a
+        rsum = rsum+mycard.points()
+    assert rsum == 13
+
+def test_real():
+    test_data = "Card   1: 99 65 21  4 72 20 77 98 27 70 | 34 84 74 18 41 45 72  2  1 75 52 47 50 93 25 10 79 87 42 69  8 12 54 96 92"
+    mycard = day04.card(test_data)
+    assert mycard.winners[0] == 99
+    assert mycard.winners[9] == 70
+    assert mycard.yours[0] == 34
+    assert mycard.yours[7] == 2
+    assert mycard.yours[8] == 1
+    assert mycard.yours[24] == 92
