c27ee446141ce4d68cdbc0b3b4617dc3e668c3a1
Glen Clayton <glen.clayton@gmail.com>
2023-12-09 16:07:23 +0000

    parse almanac done

--- day05.py
+++ day05.py
@@ -16,10 +16,51 @@ class source_destination():
 
 
 class agri_map():
+
+    def parseHeader(self, block):
+        txt = block[0].strip().split(' ')[0].split('-')
+        self.fromItem = txt[0]
+        assert txt[1] == 'to'
+        self.toItem = txt[2]
+
+    def parseItems(self, block):
+        self.maps = []
+        for row in block[1:]:
+            rowData = row.strip().split(' ')
+            assert len(rowData) == 3
+            self.maps.append(source_destination(int(rowData[0]), int(rowData[1]), int(rowData[2])))
+
+
     def parse(self, block):
         self.parseHeader(block)
+        self.parseItems(block)
         
     def __init__(self, block):
         self.parse(block)
 
+class almanac:
+    
+    def parseSeeds(self, line):
+        l = line.split(':')[1].strip()
+        s_seeds = l.split(' ')
+        self.seeds = [int(x) for x in s_seeds]
+
+
+    def parseBlock(self, block):
+        self.parseSeeds(block[0])
+        runningBlock = []
+        self.agri_maps={}
+        for line in block[2:]:
+            if len(line.strip()) == 0:
+                am = agri_map(runningBlock)
+                self.agri_maps[am.fromItem]=am
+                runningBlock = []
+            else:
+                runningBlock.append(line)
+        if len(runningBlock) > 0 :
+            am = agri_map(runningBlock)
+            self.agri_maps[am.fromItem]=am
+
+    def __init__(self, block):
+        self.parseBlock(block)
 
--- test_day05.py
+++ test_day05.py
@@ -2,39 +2,39 @@ import pytest
 import day05
 
 def data():
-    test_data ="""seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4"""
+    test_data =["seeds: 79 14 55 13",
+"",
+"seed-to-soil map:",
+"50 98 2",
+"52 50 48",
+"",
+"soil-to-fertilizer map:",
+"0 15 37",
+"37 52 2",
+"39 0 15",
+"",
+"fertilizer-to-water map:",
+"49 53 8",
+"0 11 42",
+"42 0 7",
+"57 7 4",
+"",
+"water-to-light map:",
+"88 18 7",
+"18 25 70",
+"",
+"light-to-temperature map:",
+"45 77 23",
+"81 45 19",
+"68 64 13",
+"",
+"temperature-to-humidity map:",
+"0 69 1",
+"1 0 69",
+"",
+"humidity-to-location map:",
+"60 56 37",
+"56 93 4"]
     return test_data
 
 def test_sourcedestination():
@@ -47,3 +47,34 @@ def test_sourcedestination():
     assert idx == -1
     idx = sd.mapSource(10)
     assert idx == -1
+
+def test_agrimapparse():
+    test_data = ['seed-to-soil map:\n','50 98 2\n','52 50 48\n']
+    am = day05.agri_map(test_data)
+    assert am.fromItem == 'seed'
+    assert am.toItem == 'soil'
+    assert len(am.maps) == 2
+    idx = am.maps[0].mapSource(98)
+    assert idx == 50
+    idx = am.maps[1].mapSource(51)
+    assert idx == 53
+
+def test_parseseeds():
+    test_data = ['seeds: 1 3 43 123\n','\n','seed-to-soil map:\n','50 98 2\n','52 50 48\n']
+    alm = day05.almanac(test_data)
+    assert len(alm.seeds) == 4
+    assert alm.seeds[0] == 1
+    assert alm.seeds[3] == 123
+
+def test_parsealmanac():
+    test_data = ['seeds: 1 3 43 123\n','\n','seed-to-soil map:\n','50 98 2\n','52 50 48\n']
+    alm = day05.almanac(test_data)
+    assert len(alm.agri_maps) == 1
+    assert alm.agri_maps['seed'].fromItem == 'seed'
+    assert alm.agri_maps['seed'].toItem == 'soil'
+
+def test_parsealmanac_full():
+    test_data = data()
+    alm = day05.almanac(test_data)
+    assert len(alm.seeds) == 4
+    assert len(alm.agri_maps) == 7
